8c6822f12f5431e34563bc2f88fca3449b693a3b
g87736954 <g87736954@gmail.com>
2019-04-09 14:41:17 +0800

    Initial Commit

---
+++ qrcosetest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  qrcosetest
+//
+//  Created by User on 2019/4/9.
+//  Copyright © 2019 g87. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
